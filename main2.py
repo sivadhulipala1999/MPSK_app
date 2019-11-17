36c76707aa237af9e6cb30dd3821ba80d237bf39
Abhinav <abhinav98jain@gmail.com>
2019-11-17 19:48:09 +0530

    Added demod

--- main2.py
+++ main2.py
@@ -6,6 +6,14 @@ from matplotlib import pyplot as plt
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
+import numpy as np
+
+fc = 2  # Carrier freq Hz
+A = 2  # V
+b = 0.08  # Transition band, as a fraction of the sampling rate (in (0, 0.5)).
+N = int(np.ceil((4 / b)))
+n = np.arange(N)
+bit_rate = 512*8
 
 class MyLayout(FloatLayout):
     name = ObjectProperty(None)
@@ -13,7 +21,9 @@ class MyLayout(FloatLayout):
 
     def btnfunc(self):
         print(self.name.text)
-        mpskmod(int(self.name.text))
+        m = int(self.name.text)
+        mod = mpskmod(m)
+        mpskdemod(m,mod)
 
 class MyNewApp(App):
     def build(self):
@@ -29,6 +39,7 @@ def mpskmod(m):
     d = d + [0] * extra_bit
 
     ind = [i for i in range(0, len(d)) if i % num_bit == 0]
+    print(ind)
 
     num_arr = []
 
@@ -46,11 +57,9 @@ def mpskmod(m):
     print(num_arr)
 
     # carrier generation
-    fc = 2  # Hz
-    A = 2  # V
 
     phases = arange(0, 2 * pi, (2 * pi) / m)
-    time = arange(0, 1 / (512) + 1, 1 / (512))
+    time = arange(0, 1 / bit_rate + 1, 1 / bit_rate)
 
     plot_val = []
     # x_val = []
@@ -59,11 +68,53 @@ def mpskmod(m):
         angle = phases[ind]  # ind is the symbol formed from the group of bits
         for t in time:
             plot_val.append(A * cos(2 * pi * fc * t + angle))
+    plt_time = arange(0, (1 / bit_rate + 1)*len(num_arr), 1 / bit_rate)
 
-    plt.plot(plot_val)
+    plt.figure("b")
+    plot_val = awgn(plot_val)
+    plt.plot(plt_time, plot_val)
     plt.grid(True)
-    plt.show()
+    return plot_val
 
+
+def awgn(d):
+    mean = 0
+    var = 0.1
+    sigma = var ** 0.5
+    gauss = np.random.normal(mean, sigma, len(d))
+    noisy = d + gauss
+    return d
+
+def mpskdemod(m, sig):
+    num_bit = int(log2(m))
+    sig = np.asarray(sig)
+    phases = arange(0, 2 * pi, (2 * pi) / m)
+    num_sym_tran = int(len(sig)/bit_rate)
+    time = arange(0, (1 / bit_rate + 1), 1 / bit_rate)
+    plt_time = arange(0, (1 / bit_rate + 1) *8, 1 / bit_rate)
+    h = np.sinc(2 * 4 * (n - (N - 1) / 2))
+    # sig = np.convolve(sig, h, 'same')*pow(10,15)
+    # plt.figure("a")
+    # plt.plot(plt_time,sig)
+    # plt.grid(True)
+    # plt.show()
+    sym_tran = np.split(sig, num_sym_tran)
+    demod = []
+    for i in sym_tran:
+        op = []
+        for j in phases:
+            temp = np.dot(np.cos(2*pi*fc*time+j), np.asarray(i))
+            op.append(temp)
+        demod.append(np.argmax(op))
+    demod_str = ""
+    for i in demod:
+        demod_str = demod_str + (np.binary_repr(i,num_bit))
+    demod_sig = []
+
+        # plt.plot(s)
+        # plt.grid(True)
+        # plt.show()
+    print("Demodulated "+demod_str)
 
 if __name__ == "__main__":
     MyNewApp().run()
